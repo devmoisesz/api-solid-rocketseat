6abbea5a479738322dce777e5aa8c21c70f98b30
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-06-20 22:45:56 -0300

    feat: repositories check-ins and gyms of prisma

--- src/repositories/in-memory/in-memory-check-repository.ts
+++ src/repositories/in-memory/in-memory-check-repository.ts
@@ -8,19 +8,19 @@ export class InMemoryCheckInsRepository implements CheckInsRepository{
 
 
     async findByUserIdOnDate(userId: string, date: Date) {
-    const startOfTheDay = dayjs(date).startOf('date')
-    const endOfTheDay = dayjs(date).endOf('date')
+        const startOfTheDay = dayjs(date).startOf('date')
+        const endOfTheDay = dayjs(date).endOf('date')
 
-    const checkInOnSameDate = this.items.find((checkIn) => {
-        const checkInDate = dayjs(checkIn.created_at)
-        const isOnSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+        const checkInOnSameDate = this.items.find((checkIn) => {
+            const checkInDate = dayjs(checkIn.created_at)
+            const isOnSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
 
-        return checkIn.user_id === userId && isOnSameDate
-    })
+            return checkIn.user_id === userId && isOnSameDate
+        })
 
-    if(!checkInOnSameDate) return null
+        if(!checkInOnSameDate) return null
 
-    return checkInOnSameDate
+        return checkInOnSameDate
     }
 
     async create(data: Prisma.CheckInUncheckedCreateInput) {
