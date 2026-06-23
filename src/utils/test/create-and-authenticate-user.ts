aefbb0e345d0fc2d36ddb941da4ede138445fbe4
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-06-23 13:47:32 -0300

    test: e2e RBAC

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,13 +1,17 @@
 import request from "supertest"
 import type { FastifyInstance } from "fastify"
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
 
-export async function createAndAuthenticateUser(app: FastifyInstance){
-    await request(app.server).post('/users').send({
-        name: 'Fulano',
-        email: 'fulano@example.com',
-        password: '232323'
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false){
+    await prisma.user.create({
+        data: {
+            name: 'Fulano',
+            email: 'fulano@example.com',
+            password_hash: await hash('232323', 6),
+            role: isAdmin ? 'ADMIN' : 'MEMBER',
+        }
     })
-
 
     const authResponse = await request(app.server).post('/authenticate').send({
         email: 'fulano@example.com',
@@ -20,3 +24,4 @@ export async function createAndAuthenticateUser(app: FastifyInstance){
         token
     }
 }
+
